76dd447d42626f67e3357a0cafef38d368bf1440
Janos Feher <i.am@janosfeher.com>
2015-02-10 13:38:09 +0000

    Lock dir added (required by php-fpm init).

--- centos-php/scripts/first_run.sh
+++ centos-php/scripts/first_run.sh
@@ -2,6 +2,7 @@ pre_start_action() {
   mkdir -p $SESSION_DIR
   mkdir -p $WSDL_CACHE_DIR
   mkdir -p $LOG_DIR
+  mkdir -p $LOCK_DIR
   /usr/bin/ssh-keygen -A
 }
 
--- centos-php/scripts/start.sh
+++ centos-php/scripts/start.sh
@@ -7,6 +7,9 @@ set -e
 SESSION_DIR=/var/lib/php/session
 LOG_DIR=/var/log/php-fpm
 WSDL_CACHE_DIR=/var/lib/php/wsdlcache
+LOCK_DIR=/var/run/lock/subsys
+
+# The main user for PHP-FPM.
 PHP_USER=apache
 PHP_GROUP=apache
 
@@ -22,6 +25,7 @@ post_start_action
 chown -R $PHP_USER:$PHP_GROUP $SESSION_DIR
 chown -R $PHP_USER:$PHP_GROUP $WSDL_CACHE_DIR
 chown -R $PHP_USER:$PHP_GROUP $LOG_DIR
+chown -R $PHP_USER:$PHP_GROUP $LOCK_DIR
 
 echo "Starting Syslog-ng..."
 syslog-ng --no-caps
